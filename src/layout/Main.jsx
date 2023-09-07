3a49c5e4eb2a344d86490548cae042fcc9cad4e7
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-09-07 20:48:25 +0600

    add footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navigation from '../shared/Navigation';
+import Footer from '../shared/Footer';
 
 const Main = () => {
     return (
         <div>
             <Navigation />
             <Outlet />
+            <Footer />
         </div>
     );
 };
---
+++ src/shared/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='p-2 text-center bg-slate-300'>
+            <p>All Rights Are Reserved By SoftwareBlog | 2023</p>
+        </div>
+    );
+};
+
+export default Footer;
