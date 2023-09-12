8b56055d728b8cee213f00863c44905a0297b162
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-09-12 10:19:17 +0600

    add pvt route

--- src/dashboard/SidebarList.jsx
+++ src/dashboard/SidebarList.jsx
@@ -36,6 +36,13 @@ const SidebarList = () => {
             <div className='flex gap-2 items-center hover:text-[#2CC8B3] justify-between hover:bg-slate-100 py-2 px-1'>
                 <div className='flex gap-2 items-center'>
                     <FaShareSquare />
+                    <Link to="/dashboard" className='inline-block text-[16px]'>Widgets</Link>
+                </div>
+                <FaArrowRight />
+            </div>
+            <div className='flex gap-2 items-center hover:text-[#2CC8B3] justify-between hover:bg-slate-100 py-2 px-1'>
+                <div className='flex gap-2 items-center'>
+                    <FaShareSquare />
                     <Link to="/dashboard" className='inline-block text-[16px]'>Logout</Link>
                 </div>
                 <FaArrowRight />
