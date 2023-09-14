738ab476a902990c19443af0aa34f2f8aa282103
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-09-14 10:54:43 +0600

    add card info

--- src/cards/FeaturedCard.jsx
+++ src/cards/FeaturedCard.jsx
@@ -2,27 +2,27 @@ import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const FeaturedCard = () => {
+const FeaturedCard = ({article}) => {
+    const {title, image, date, info} = article;
     return (
         <Card>
             <CardMedia
                 component="img"
                 alt="green iguana"
                 height="140"
-                image="https://images.wondershare.com/drfone/article/2022/08/how-to-bypass-icloud-activation-lock-screen-on-ios-16-01.jpg"
+                image={image}
             />
             <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                    Lizard
+                    {title}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
+                    {info}
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small"><Link to="/details">Learn More</Link></Button>
+                <Button size="small">{date}</Button>
+                <Button size="small"><Link to="/details">Read More</Link></Button>
             </CardActions>
         </Card>
     );
